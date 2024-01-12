bc59fe820136ebbc05be70f16a60c7c297c3d1f6
kaybrian <kayonbrian@gmail.com>
2024-01-12 12:11:34 +0000

    updated the code to add only matrixes of rhe same shape

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,7 +10,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
         A function def cat_matrices2D(mat1, mat2, axis=0):
         that concatenates two matrices along a specific axis:
     '''
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
-    else:
+    elif axis == 1 and len(mat1) == len(mat2):
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    else:
+        return None
