a820d0397a299f67a4fa9b467e98d9d9b48fe779
kaybrian <kayonbrian@gmail.com>
2024-01-23 10:53:00 +0200

    histograph

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,7 +6,7 @@ np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
 # your code here
-plt.hist(student_grades, bins=10,edgecolor='black', range=(0, 100))
+plt.hist(student_grades, bins=10, edgecolor='black', range=(0, 100))
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
