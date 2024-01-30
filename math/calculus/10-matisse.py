0cddef5cf95d83446c7b473fbc8e14dab18279c1
kaybrian <kayonbrian@gmail.com>
2024-01-30 16:24:07 +0200

    a little change

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,6 +9,8 @@ def poly_derivative(poly):
     '''
         calculates the derivative of a polynomial
     '''
-    if type(poly) is not list or len(poly) <= 1:
+    if type(poly) is not list:
+        return None
+    elif len(poly) <= 1:
         return [0]
     return [poly[i] * i for i in range(1, len(poly))]
