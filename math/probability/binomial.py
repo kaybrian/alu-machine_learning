cad43534ee35048e3696ad9ddc9f116c285ff2cf
kaybrian <kayonbrian@gmail.com>
2024-02-06 13:13:51 +0200

    initialize binomal

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -13,16 +13,27 @@ class Binomial:
             Class constructor
         '''
         if data is None:
-            if n <= 0:
-                raise ValueError('n must be a positive value')
+            if n < 1:
+                raise ValueError("n must be a positive value")
+            else:
+                self.n = n
             if p <= 0 or p >= 1:
-                raise ValueError('p must be greater than 0 and less than 1')
-            self.n = int(n)
-            self.p = float(p)
+                raise ValueError("p must be greater than 0 and less than 1")
+            else:
+                self.p = p
         else:
             if type(data) is not list:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            self.n = int(max(data))
-            self.p = float(sum(data) / len(data))
+            mean = float(sum(data) / len(data))
+            summation = 0
+            for x in data:
+                summation += ((x - mean) ** 2)
+            variance = (summation / len(data))
+            q = variance / mean
+            p = (1 - q)
+            n = round(mean / p)
+            p = float(mean / n)
+            self.n = n
+            self.p = p
