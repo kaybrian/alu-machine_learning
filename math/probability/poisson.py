9a2e0ea7363b7f3e94ecae2bafb088bfad0e5847
kaybrian <kayonbrian@gmail.com>
2024-02-06 11:40:27 +0200

    create a class called poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+    Poisson distribution
+    that represents a poisson distribution
+'''
+
+
+class Poisson:
+    '''
+        Class Poisson that represents a
+        distribution of Poisson
+    '''
+    def __init__(self, data=None, lambtha=1.):
+        '''
+            Class constructor
+        '''
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(sum(data) / len(data))
