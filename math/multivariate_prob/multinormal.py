dac9c3d65180845b946849564d3ad9d6fcb9d679
kaybrian <kayonbrian@gmail.com>
2024-03-04 23:56:48 +0200

    format

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -21,15 +21,13 @@ class MultiNormal:
             containing the data set
         """
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-            raise TypeError("X must be a 2D numpy.ndarray")
+            raise TypeError("data must be a 2D numpy.ndarray")
 
-        if data.shape[0] < 2:
+        n, d = data.shape
+        if n < 2:
             raise ValueError("data must contain multiple data points")
 
         self.data = data
-
-        # Set the public instance variables:
-        d, n = data.shape
         mean = np.mean(data, axis=1, keepdims=True)
         self.mean = mean
         cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
