4f7ce8af01a0313a41f46e3b39a8a89b42efda15
kaybrian <kayonbrian@gmail.com>
2024-03-04 23:29:32 +0200

    correlation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -22,7 +22,7 @@ def correlation(C):
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
 
-    if C.shape[0]!= C.shape[1]:
+    if C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     d = C.shape[0]
