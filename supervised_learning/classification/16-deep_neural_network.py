a643e88d163914cd7ffb2e8ba9f6e9b7928928a2
kaybrian <kayonbrian@gmail.com>
2024-05-17 10:21:55 +0200

    only one for loop

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -4,7 +4,6 @@
     network performing binary classification
 """
 
-
 import numpy as np
 
 
@@ -14,21 +13,12 @@ class DeepNeuralNetwork:
     """
 
     def __init__(self, nx, layers):
-        if isinstance(nx, int) is False:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        if isinstance(layers, list) is False and len(layers) == 0:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
-        for i in layers:
-            if isinstance(i, int) is False:
-                raise TypeError("layers must be a list of positive integers")
-
-            if i < 1:
-                raise ValueError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
@@ -36,12 +26,13 @@ class DeepNeuralNetwork:
         self.nx = nx
         self.layers = layers
 
-        # init the weights and the biases
-        for ii in range(self.L):
+        # Initialize weights and biases and validate layers in one loop
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
             if i == 0:
                 self.weights["W1"] = (
-                    np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
-                )
+                    np.random.randn(layers[i], nx) * np.sqrt(2 / nx))
             else:
                 self.weights["W" + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]
