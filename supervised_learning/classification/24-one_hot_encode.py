ea262de613659806fd9a1e3167c364a8ea895c5a
kaybrian <kayonbrian@gmail.com>
2024-05-17 13:25:46 +0200

    hot encode

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -9,20 +9,20 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """
-        one hot encode function
-        Args:
-            Y: numpy.ndarray of shape (classes,)
-            classes: number of classes
-        Returns:
-            one-hot encoding of Y with shape (classes, m)
-            m: number of examples
+    one hot encode function
+    Args:
+        Y: numpy.ndarray of shape (classes,)
+        classes: number of classes
+    Returns:
+        one-hot encoding of Y with shape (classes, m)
+        m: number of examples
     """
-    if type(Y) is not np.ndarray or len(Y) == 0:
+    if type(Y) is not np.ndarray:
         return None
-    if type(classes) is not int or classes <= 0:
+    if type(classes) is not int:
         return None
-    
-    one_hot = np.zeros((classes, Y.shape[0]))
-    for i in range(Y.shape[0]):
-        one_hot[Y[i], i] = 1
-    return one_hot
+    try:
+        one_hot = np.eye(classes)[Y].transpose()
+        return one_hot
+    except Exception as err:
+        return None
