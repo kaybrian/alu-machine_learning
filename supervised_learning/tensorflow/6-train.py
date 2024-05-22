e4fec58d44687ead4611514b00b3537994b699bd
kaybrian <kayonbrian@gmail.com>
2024-05-22 20:49:39 +0200

    modules

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -7,13 +7,11 @@
 
 
 import tensorflow as tf
-
-calculate_accuracy = __import__("3-calculate_accuracy").calculate_accuracy
-calculate_loss = __import__("4-calculate_loss").calculate_loss
-create_placeholders = __import__("0-create_placeholders").create_placeholders
-create_train_op = __import__("5-create_train_op").create_train_op
-forward_prop = __import__("2-forward_prop").forward_prop
-
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
 
 def train(
     X_train,
