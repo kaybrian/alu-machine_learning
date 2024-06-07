18c3708ec8b4b82e164bdfcfc19e9a0fbf361fd0
kaybrian <kayonbrian@gmail.com>
2024-06-07 14:20:13 +0200

    return the inverse

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-'''
+"""
     Function def learning_rate_decay(
         alpha, decay_rate, global_step, decay_step
     ):
     that updates the learning rate using inverse time decay in numpy
-'''
+"""
 
 
 import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    '''
-        Updates the learning rate using inverse time decay in numpy
+    """
+    creates a learning rate decay operation in tensorflow
+    using inverse time decay:
 
-        Args:
-            - alpha is the original learning rate
-            - decay_rate is the decay rate
-            - global_step is the number of passes of gradient
-            descent that have elapsed
-            - decay_step is the number of passes of gradient
-            descent that should occur
-            before the learning rate is decayed
-            - the learning rate decay should occur in a stepwise
-            fashion
+    Args:
+        - alpha is the original learning rate
+        - decay_rate is the weight used to determine the
+        rate at which alpha will decay
+        - global_step is the number of passes of gradient
+        descent that have elapsed
+        - decay_step is the number of passes of gradient
+        descent that should occur before alpha is decayed further
+        - the learning rate decay should occur in a stepwise fashion
 
-        Returns:
-            The updated learning rate
-    '''
-    return alpha / (1 + decay_rate * tf.floor(global_step / decay_step))
+    Returns:
+        - the learning rate decay operation
+    """
+    return tf.train.inverse_time_decay(
+        alpha, global_step, decay_step, decay_rate
+    )
