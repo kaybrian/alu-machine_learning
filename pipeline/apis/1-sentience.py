7798461a6d88f48189d6e424eb8d1ad870738b3c
kaybrian <kayonbrian@gmail.com>
2024-06-29 16:32:02 +0200

    error

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -12,33 +12,18 @@ def sentientPlanets():
     Return the list of names of the home
     planets of all sentient species.
     '''
-    planets = []
-
-    try:
-        url = 'https://swapi-api.alx-tools.com/api/species/'
-
-        while url:
-            response = requests.get(url)
-            data = response.json()
-            species = data['results']
-
-            for specie in species:
-                if (
-                    specie['classification'] == 'sentient' or
-                    specie['designation'] == 'sentient'
-                ):
-                    homeworld_url = specie['homeworld']
-
-                    if homeworld_url:
-                        homeworld_response = requests.get(homeworld_url)
-                        homeworld_response.raise_for_status()
-                        homeworld_data = homeworld_response.json()
-                        planets.append(homeworld_data['name'])
-
-            url = data['next']
-
-        return planets
-    except requests.RequestException as e:
-        print('An error occurred: {}'.format(e))
-    except Exception as e:
-        print('A generale occurred: {}'.format(e))
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
