248becb8dc44f099f6aa6fcdf7c297940f44ba22
kaybrian <kayonbrian@gmail.com>
2024-07-05 13:15:52 +0200

    city temps

---
+++ pipeline/databases/6-avg_temperatures.sql
@@ -0,0 +1,11 @@
+-- Script that displays the average temperature (Fahrenheit)
+--  by city ordered by temperature (descending).
+SELECT
+    city,
+    AVG(value) AS avg_temp
+FROM
+    hbtn_0c_0
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC;
