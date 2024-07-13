6ad54adf0176baa937443ee021f85726223949c6
kaybrian <kayonbrian@gmail.com>
2024-07-13 10:51:12 +0200

    old school band

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -5,8 +5,3 @@
 --     Import this table dump: metal_bands.sql.zip
 --     Column names must be: origin and nb_fans
 --     Your script can be executed on any database
-
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
