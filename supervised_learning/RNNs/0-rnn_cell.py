08016eb3e5e245967e2f8605107ea77d6502bda1
kaybrian <kayonbrian@gmail.com>
2024-09-13 18:41:02 +0200

    rns

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
+"""
+    Script that defines a class RNNCell
+    that defines a simple recurrent neural network cell
+"""
+
 
 import numpy as np
-RNNCell = __import__('0-rnn_cell').RNNCell
 
-np.random.seed(0)
-rnn_cell = RNNCell(10, 15, 5)
-print("Wh:", rnn_cell.Wh)
-print("Wy:", rnn_cell.Wy)
-print("bh:", rnn_cell.bh)
-print("by:", rnn_cell.by)
-rnn_cell.bh = np.random.randn(1, 15)
-rnn_cell.by = np.random.randn(1, 5)
-h_prev = np.random.randn(8, 15)
-x_t = np.random.randn(8, 10)
-h, y = rnn_cell.forward(h_prev, x_t)
-print(h.shape)
-print(h)
-print(y.shape)
-print(y)
+
+class RNNCell:
+    """
+    Class that defines a simple recurrent neural network cell
+    """
+
+    def __init__(self, i, h, o):
+        """
+        Class constructor
+        """
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+        self.Wh = np.random.normal(size=(h + i, h))
+        self.Wy = np.random.normal(size=(h, o))
+
+    def softmax(self, x):
+        """
+        Performs the softmax function
+
+        parameters:
+            x: the value to perform softmax on to generate output of cell
+
+        return:
+            softmax of x
+        """
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        softmax = e_x / e_x.sum(axis=1, keepdims=True)
+        return softmax
+
+    def forward(self, h_prev, x_t):
+        """
+        Function that performs forward propagation
+        """
+        concatenation = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concatenation, self.Wh) + self.bh)
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
+        return h_next, y
